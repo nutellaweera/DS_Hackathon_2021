a07c7e7bbbe3c7353ae4772f91613600f2cc11a3
Sunella Uni <8774488+nutellaweera@users.noreply.github.com>
2021-11-18 21:34:29 +0000

    Emissions extended.

--- Emissions/emissions.py
+++ Emissions/emissions.py
@@ -7,14 +7,9 @@ filtered = df[(df["country"].isin(["India", "China", "Ethiopia", "United States"
 world = df[(df["country"].isin(["World"])) & (df["year"]<2000) & (df["year"]>1899)].filter(items=["year", "population"])
 filtered.set_index('country', inplace=True)
 
-share_of_pop = []
-for index, row in filtered.iterrows():
-    world_pop = world.loc[world["year"] == row["year"]]["population"].iloc[0]
-    share_of_pop.append(row["population"]/world_pop * 100)
-
-filtered["share_of_population"] = share_of_pop
 filtered["share_global_co2"] = filtered["share_global_co2"].fillna(0)
 filtered.to_csv('Emissions/CO2_countries_filtered.csv')
 
 graph = sns.scatterplot(data=filtered, x="year", y="share_global_co2", hue="country", size="population", sizes=(20,800))
+graph.set_ylabel("Share of global CO2 (%)")
 plt.show()
